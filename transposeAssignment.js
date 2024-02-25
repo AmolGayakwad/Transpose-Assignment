9396a3c2b610263d4ac817de75eaf63c986a54d5
AmolGayakwad <158410064+AmolGayakwad@users.noreply.github.com>
2024-02-25 11:43:31 +0530

    Update transposeAssignment.js

--- transposeAssignment.js
+++ transposeAssignment.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
-// Step 1: Create an empty dictionary called 'patient_info'
 let patient_info = {};
 
-// Step 2: Read CSV one row at a time
+// Read CSV one row at a time
 fs.createReadStream('input.csv')
   .pipe(csv())
   .on('data', (row) => {
@@ -12,11 +11,8 @@ fs.createReadStream('input.csv')
     let patientID = row['Patient'];
     let fields = row['Fields'];
 
-
-    // Step 2b: Create a key by concatenating 'Patient' ID with 'Fields' value
     let key = `${patientID}_${fields}`;
 
-    // Step 2c: Add key-value pair to 'patient_info' dictionary
     patient_info[key] = row['Value']; 
   })
   .on('end', () => {
@@ -31,7 +27,6 @@ fs.createReadStream('input.csv')
     }
     let headerRow = ['Patient', ...Array.from(fieldsSet)];
 
-    // Step 4: Write header row to output file
     fs.writeFileSync('output.csv', headerRow.join(',') + '\n');
 
     for (let patientId of uniquePatientIDs) {
@@ -40,7 +35,7 @@ fs.createReadStream('input.csv')
       for (let field of fieldsSet) {
         let key = `${patientId}_${field}`;
         console.log(key)
-        dataRow.push(patient_info[key] || ''); // If the value is missing, default to an empty string
+        dataRow.push(patient_info[key] || ''); 
         console.log(dataRow)
       }
   
